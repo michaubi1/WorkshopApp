f6576a6cb96456c1158e3ce6fc5ce3c5ea7289c5
michalbieganski <272620@student.pwr.edu.pl>
2023-10-20 10:23:04 +0100

    Merged our projects

--- src/org/ulpgc/is1/model/Vehicle.java
+++ src/org/ulpgc/is1/model/Vehicle.java
@@ -8,4 +8,11 @@ public class Vehicle {
     private Plate plate;
     private Customer owner;
     private List<Repair> repairs;
+
+    public Vehicle(String make, String model, Plate plate, Customer owner) {
+        this.make = make;
+        this.model = model;
+        this.plate = plate;
+        this.owner = owner;
+    }
 }
